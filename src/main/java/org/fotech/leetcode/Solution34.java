3f4586361f255d8b9afc07a9f8b266ee697d569d
Forri <yefengruio1996@gmail.com>
2023-06-04 21:46:35 +0800

    feat solution35

---
+++ src/main/java/org/fotech/leetcode/Solution34.java
@@ -0,0 +1,27 @@
+package org.fotech.leetcode;
+
+/**
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ *
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ */
+public class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                result[0] = i;
+                break;
+            }
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == target) {
+                result[1] = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/org/fotech/leetcode/Solution35.java
@@ -0,0 +1,63 @@
+package org.fotech.leetcode;
+
+import org.fotech.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 请实现 copyRandomList 函数，复制一个复杂链表。
+ * 在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，
+ * 还有一个 random 指针指向链表中的任意节点或者 null。
+ */
+public class Solution35 {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            Node node = new Node(head.val, null);
+            node.random = head.random == null ? null : node;
+            return node;
+        }
+        // 先复制一个简单的链表
+
+        // 先new一个头对象
+        // 然后遍历curr。每遍历一次，就要new一个新的node，然后赋值val给新的node，然后设置为node。next
+        // curr 应该领先head一个
+        // 维护一个map，key是老node，value是新的node
+        Node curr = head.next;
+        Node resultHead = new Node(head.val);
+        Node result = resultHead;
+        // 老node和新node的映射关系
+        Map<Node, Node> nodeMap = new HashMap<>();
+        nodeMap.put(head, resultHead);
+        while (curr!= null) {
+            Node item = new Node(curr.val);
+            nodeMap.put(curr, item);
+            resultHead.next = item;
+            resultHead = resultHead.next;
+            curr = curr.next;
+        }
+        curr = head;
+        resultHead = result;
+        while (curr != null) {
+            if (curr.random != null) {
+                resultHead.random = nodeMap.get(curr.random);
+            }
+            curr = curr.next;
+            resultHead = resultHead.next;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution35 solution35 = new Solution35();
+        Node node1 = new Node(1);
+        node1.random = node1;
+
+        Node res = solution35.copyRandomList(node1);
+        System.out.println(res);
+    }
+}
