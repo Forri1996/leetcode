f61f54fee32426b6de054e6ef06acaeee77ed7ed
Forri <yefengrui1996@gmail.com>
2023-09-17 14:21:20 +0800

    feat 每周二题

---
+++ src/main/java/org/fotech/leetcode/Solution933.java
@@ -0,0 +1,66 @@
+package org.fotech.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+/**
+ * 写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+ *
+ * 请你实现 RecentCounter 类：
+ *
+ * RecentCounter() 初始化计数器，请求数为 0 。
+ * int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+ * 保证 每次对 ping 的调用都使用比之前更大的 t 值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["RecentCounter", "ping", "ping", "ping", "ping"]
+ * [[], [1], [100], [3001], [3002]]
+ * 输出：
+ * [null, 1, 2, 3, 3]
+ *
+ * 解释：
+ * RecentCounter recentCounter = new RecentCounter();
+ * recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+ * recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+ * recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+ * recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回 3
+ */
+public class Solution933 {
+    Queue<Integer> queue;
+    public Solution933() {
+        queue = new ArrayDeque<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+        while (true) {
+            int m = queue.peek();
+            if (t - m > 3000) {
+                queue.poll();
+            } else {
+                break;
+            }
+        }
+
+        return queue.size();
+    }
+
+    public static void main(String[] args) {
+        Solution933 solution933 = new Solution933();
+        System.out.println(solution933.ping(1));
+        System.out.println(solution933.ping(100));
+        System.out.println(solution933.ping(3001));
+        System.out.println(solution933.ping(3002));
+    }
+}
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+
