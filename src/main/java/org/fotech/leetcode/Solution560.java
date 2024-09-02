5692ab19d42ea24a3d7a6b76fa63f68516e6bdb9
Forri <yefengrui1996@gmail.com>
2024-09-02 23:57:02 +0800

    feat  一周二题 560

--- src/main/java/org/fotech/leetcode/Solution560.java
+++ src/main/java/org/fotech/leetcode/Solution560.java
@@ -6,24 +6,34 @@ import java.util.Map;
 // todo un ac
 // 前缀树
 public class Solution560 {
-    public static int calNum(int[] inputArr, int k) {
+    /**
+     * 前缀和
+     * @param inputArr
+     * @param k
+     * @return
+     */
+    public int calNum(int[] inputArr, int k) {
         int result = 0;
-        int[] preSum = new int[inputArr.length];
         Map<Integer, Integer> preSumMap = new HashMap<>();
+        preSumMap.put(0, 1);
+
         int sum = 0;
         // 构建前缀和数组
         for (int i = 0; i < inputArr.length; i++) {
             sum += inputArr[i];
-            preSum[i] = sum;
+            // 这里的判断要前置，因为判断的子串需要在左边
+            if (preSumMap.containsKey(sum)) {
+                result += preSumMap.get(sum);
+            }
             preSumMap.put(sum, preSumMap.getOrDefault(sum, 0) + 1);
-        }
-        if (preSum[0] == k) {
-            result++;
-        }
-        for (int i = 1; i < inputArr.length; i++) {
-            result += preSumMap.getOrDefault(k-preSum[i], 0);
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution560 solution560 = new Solution560();
+        int[] input = new int[]{1,2,3};
+        solution560.calNum(input, 3);
     }
 }
