e915ca15c5642607e73a2e237d0176d537f34468
Forri <yefengrui1996@gmail.com>
2024-09-19 21:44:28 +0800

    feat  一周二题 560

--- src/main/java/org/fotech/hot100/Solution53.java
+++ src/main/java/org/fotech/hot100/Solution53.java
@@ -4,16 +4,26 @@ public class Solution53 {
     private int maxArr(int[] m) {
         int result = 0;
         int pre = 0;
+        int[] k = new int[m.length];
         for (int i = 0; i< m.length; i++) {
             if (i == 0) {
                 pre = m[i];
                 result = m[i];
             } else {
-                result = Math.max(pre, pre+result);
-                pre = result;
+                pre = Math.max(m[i], pre+m[i]);
+                result = Math.max(pre,result);
             }
+            k[i] = result;
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution53 solution53 = new Solution53();
+        // -2,1,-2,4,4,3,5,6,1,5
+        int[] input = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+
+        System.out.println(solution53.maxArr(input));
     }
 }
