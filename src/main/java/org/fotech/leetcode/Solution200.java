c194bf1d65e0e33eae6f745fbf1a09867b3321f4
Forri <yefengrui1996@gmail.com>
2024-12-04 09:07:48 +0800

    华为 leetcode

---
+++ src/main/java/org/fotech/leetcode/Solution200.java
@@ -0,0 +1,58 @@
+package org.fotech.leetcode;
+
+/**
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ */
+public class Solution200 {
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    result++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+
+    return result;
+    }
+
+    /**
+     * 从陆地的一角进来，把这块陆地全部设置成0
+     * @param grid
+     * @param i 横向
+     * @param j 竖项
+     */
+    private void dfs(char[][] grid, int i, int j) {
+        // 边界情况
+        if (i < 0 || i > grid.length || j < 0 || j > grid[0].length) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+    }
+}
