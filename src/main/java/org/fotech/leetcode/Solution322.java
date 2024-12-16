364ae5100e965cd613e8e5f150ead7cbb44090f8
yefengrui <yefr@chinatelecom.cn>
2024-12-16 15:03:42 +0800

    硬币问题

--- src/main/java/org/fotech/leetcode/Solution322.java
+++ src/main/java/org/fotech/leetcode/Solution322.java
@@ -24,4 +24,26 @@ package org.fotech.leetcode;
  * 输出：0
  */
 public class Solution322 {
+    public static void main(String[] args) {
+        int[] coins = {1,2,5};
+        int amount = 11;
+
+        int[] dp = new int[amount+1];
+        /**
+         * dp[0] = 0
+         * dp[i]=min(dp[i],dp[i-coin]+1)?
+         */
+        dp[0] = 0;
+        for (int i = 1; i <=amount; i++) {
+            for (int coin : coins) {
+                if (dp[i] == 0 && i%coin == 0) {
+                    dp[i] = i/coin;
+                }
+                if (i-coin > 0) {
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                }
+            }
+        }
+        System.out.println(dp[amount]);
+    }
 }
