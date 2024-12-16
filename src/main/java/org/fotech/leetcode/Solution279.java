2ffecd0ddd450f4ad03abe7865369ab7128e3ddc
yefengrui <yefr@chinatelecom.cn>
2024-12-16 20:00:11 +0800

    给你一个整数 n ，返回 和为 n 的完全平方数的最少数量

--- src/main/java/org/fotech/leetcode/Solution279.java
+++ src/main/java/org/fotech/leetcode/Solution279.java
@@ -1,5 +1,8 @@
 package org.fotech.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
  *
@@ -23,4 +26,51 @@ package org.fotech.leetcode;
  * 1 <= n <= 104
  */
 public class Solution279 {
+    public static void main(String[] args) {
+        // 1 4 9 16
+        List<Integer> wqpfArr = new ArrayList<>();
+        for (int i = 1; i <=10; i++) {
+            wqpfArr.add(i*i);
+        }
+        int n = 12;
+        /**
+         * dp[1] = 1
+         * dp[2] = 2
+         * dp[3] = 3
+         * dp[4] = 1
+         * dp[5] = dp[4]+dp[1]
+         * ……
+         * dp[7] = dp[4] + dp[3]
+         * dp[8] = dp[4] + dp[4]
+         * dp[i] = dp[i-m] + dp[m]
+         */
+        int[] dp = new int[105];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        dp[4]=1;
+        if (n <= 4) {
+            System.out.println(dp[n]);
+            return;
+        }
+        for (int i = 5; i <= n; i++) {
+            if (wqpfArr.contains(i)) {
+                dp[i] = 1;
+                continue;
+            }
+            // 找到离i最近的一个最大平方数
+            for (int m = 1; m < wqpfArr.size(); m++) {
+                if (wqpfArr.get(m) > i) {
+                    break;
+                }
+                if (dp[i] == 0) {
+                    dp[i] = dp[i-wqpfArr.get(m)] + dp[wqpfArr.get(m)];
+                } else {
+                    dp[i] = Math.min(dp[i], dp[i-wqpfArr.get(m)] + dp[wqpfArr.get(m)]);
+                }
+            }
+
+        }
+        System.out.println(dp[n]);
+    }
 }
