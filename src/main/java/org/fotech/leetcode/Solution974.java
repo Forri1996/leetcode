2b81500fe84c7634a3e235bae946025eb81272f3
yefengrui <yefr@chinatelecom.cn>
2024-12-19 11:24:49 +0800

    给定一个整数数组 nums 和一个整数 k ，返回其中元素之和可被 k 整除的非空 子数组 的数目。

--- src/main/java/org/fotech/leetcode/Solution974.java
+++ src/main/java/org/fotech/leetcode/Solution974.java
@@ -18,6 +18,37 @@ package org.fotech.leetcode;
  *
  * 输入: nums = [5], k = 9
  * 输出: 0
+ *
+ * 两个值除与k的的余数相同，则他们的差可以被k整除
  */
 public class Solution974 {
+    public static void main(String[] args) {
+        int k = 5;
+        int[] nums = {4,5,0,-2,-3,1};
+        cal(nums, k);
+    }
+
+    public static void cal(int[] nums, int k) {
+        int[] qianzhuihe = new int[nums.length];
+        int sum = 0;
+        for (int i = 0;i<nums.length; i++) {
+            sum+=nums[i];
+            qianzhuihe[i] = sum;
+        }
+        int count = 0;
+        for (int i = 0; i < qianzhuihe.length; i++) {
+            for (int j = i; j < qianzhuihe.length; j++) {
+                if (i == j) {
+                    if (qianzhuihe[i] % k == 0) {
+                        count++;
+                    }
+                } else {
+                    if (qianzhuihe[j] % k == qianzhuihe[i] % k) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
