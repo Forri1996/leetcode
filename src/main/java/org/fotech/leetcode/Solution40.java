75ada8a15f1c5414d35b578746046d0d5562525d
yefengrui <yefengrui1996@gmail.com>
2024-12-23 22:32:14 +0800

    回溯2题

--- src/main/java/org/fotech/leetcode/Solution40.java
+++ src/main/java/org/fotech/leetcode/Solution40.java
@@ -1,5 +1,8 @@
 package org.fotech.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /**
  * 给定一个候选人编号的集合 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
  *
@@ -29,4 +32,35 @@ package org.fotech.leetcode;
  * ]
  */
 public class Solution40 {
+    public static void main(String[] args) {
+        int[] input = {10,1,2,7,6,1,5};
+        int target = 8;
+        System.out.println(combinationSum(input, target));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] input, int target) {
+        Arrays.sort(input);
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), input, target, -1);
+
+        return result;
+    }
+
+    private static  void backtrack(List<List<Integer>> result, List<Integer> es, int[] input, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            if (!result.contains(es)) {
+                result.add(new ArrayList<>(es));
+            }
+        } else {
+            for (int i = start+1; i < input.length; i++) {
+
+                es.add(input[i]);
+                backtrack(result, es, input, remain-input[i], i);
+                es.remove(es.size()-1);
+            }
+        }
+    }
+
 }
--- src/main/java/org/fotech/leetcode/Solution46.java
+++ src/main/java/org/fotech/leetcode/Solution46.java
@@ -1,5 +1,8 @@
 package org.fotech.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
  *
@@ -19,4 +22,29 @@ package org.fotech.leetcode;
  * 输出：[[1]]
  */
 public class Solution46 {
+    public static void main(String[] args) {
+        Solution46 solution46 = new Solution46();
+        int[] input = {1,2,3};
+        System.out.println(solution46.permute(input));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> item, int[] nums) {
+        if (item.size() == nums.length) {
+            result.add(new ArrayList<>(item));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (!item.contains(nums[i])) {
+                    item.add(nums[i]);
+                    backtrack(result, item, nums);
+                    item.remove(item.size() - 1);
+                }
+            }
+        }
+    }
 }
