6df824da9b0692fa995c167133a91389f03c0035
yefengrui <yefengrui1996@gmail.com>
2024-12-23 23:03:35 +0800

    回溯1题

--- src/main/java/org/fotech/leetcode/Solution47.java
+++ src/main/java/org/fotech/leetcode/Solution47.java
@@ -1,5 +1,9 @@
 package org.fotech.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
  *
@@ -19,4 +23,31 @@ package org.fotech.leetcode;
  *
  */
 public class Solution47 {
+    public static void main(String[] args) {
+        Solution47 sol = new Solution47();
+        int[] input = {1,1,2};
+        System.out.println(sol.permuteUnique(input));
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        backtrack(result, new ArrayList<>(), nums, new HashSet<>());
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> objects, int[] nums, HashSet<Integer> cur) {
+        if (objects.size() == nums.length) {
+            if (!result.contains(objects)) {
+                result.add(new ArrayList<>(objects));
+            }
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (cur.contains(i)) continue;
+                objects.add(nums[i]);
+                cur.add(i);
+                backtrack(result, new ArrayList<>(objects), nums, cur);
+                objects.remove(objects.size() - 1);
+                cur.remove(i);
+            }
+        }
+    }
 }
