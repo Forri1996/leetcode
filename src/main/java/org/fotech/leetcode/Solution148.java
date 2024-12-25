385f9980fdf7cdcceccbb1a4d8154d9ebd31a8ee
yefengrui <yefengrui1996@gmail.com>
2024-12-25 21:39:29 +0800

    空白题

---
+++ src/main/java/org/fotech/leetcode/Solution148.java
@@ -0,0 +1,22 @@
+package org.fotech.leetcode;
+
+/**
+ * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ *
+ *给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ *
+ *输入：head = [4,2,1,3]
+ * 输出：[1,2,3,4]
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution148 {
+}
--- src/main/java/org/fotech/leetcode/Solution98.java
+++ src/main/java/org/fotech/leetcode/Solution98.java
@@ -10,6 +10,22 @@ package org.fotech.leetcode;
  * 只包含 小于 当前节点的数。
  * 节点的右子树只包含 大于 当前节点的数。
  * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 public class Solution98 {
 }
