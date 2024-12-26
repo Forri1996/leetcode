524ec6f9d0bea1cbda023f16397073dd0b893c6c
yefengrui <yefengrui1996@gmail.com>
2024-12-26 21:46:55 +0800

    二叉树中序遍历

--- src/main/java/org/fotech/leetcode/Solution98.java
+++ src/main/java/org/fotech/leetcode/Solution98.java
@@ -12,6 +12,11 @@ package org.fotech.leetcode;
  * 所有左子树和右子树自身必须也是二叉搜索树。
  *
  */
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -28,4 +33,34 @@ package org.fotech.leetcode;
  * }
  */
 public class Solution98 {
+    static boolean flag = true;
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(-2147483648);
+        Deque<TreeNode> stack = new LinkedList<>();
+        int min = Integer.MIN_VALUE;
+        while (!stack.isEmpty() || treeNode != null) {
+            // 找到最左子树
+            while(treeNode != null) {
+                stack.push(treeNode);
+                treeNode = treeNode.left;
+            }
+            TreeNode left = stack.pop();
+            if (min != Integer.MIN_VALUE &&min >= left.val) {
+                flag = false;
+            }
+            min = left.val;
+            treeNode = left.right;
+        }
+        System.out.println(flag);
+    }
+
+    private static void dfs(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        if (treeNode.left != null && treeNode.left.val > treeNode.val) flag = false;
+        if (treeNode.right != null && treeNode.right.val < treeNode.val) flag = false;
+        dfs(treeNode.left);
+        dfs(treeNode.right);
+    }
 }
