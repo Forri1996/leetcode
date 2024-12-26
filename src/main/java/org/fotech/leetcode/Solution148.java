3e15332ad09f58da73428dae36d5a5b6ccf87970
yefengrui <yefengrui1996@gmail.com>
2024-12-26 22:23:16 +0800

    链表的归并排序（未实现） 深拷贝+dfs

--- src/main/java/org/fotech/leetcode/Solution148.java
+++ src/main/java/org/fotech/leetcode/Solution148.java
@@ -8,6 +8,13 @@ package org.fotech.leetcode;
  *输入：head = [4,2,1,3]
  * 输出：[1,2,3,4]
  */
+
+import org.fotech.bytedance.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -17,6 +24,25 @@ package org.fotech.leetcode;
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ *
+ * 归并排序
+ * 快慢指针找中点 未实现该算法
  */
 public class Solution148 {
+    public ListNode sortList(ListNode head) {
+        List<Integer> m = new ArrayList<>();
+        ListNode root = head;
+        while (root != null) {
+            m.add(root.val);
+            root = root.next;
+        }
+        List<Integer> sorted = m.stream().sorted().collect(Collectors.toList());
+        ListNode newHead = head;
+        for (int e : sorted) {
+            newHead.val = e;
+            newHead = newHead.next;
+        }
+
+        return head;
+    }
 }
